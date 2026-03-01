da395ed5759e5b9cd19710f43bc176ea3b936128
DzaakhIdn <dzaakhidn@gmail.com>
2026-03-01 14:58:59 +0700

    pop

--- lib/screens/trip_detail.dart
+++ lib/screens/trip_detail.dart
@@ -1,23 +1,27 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconify_flutter/iconify_flutter.dart';
 import 'package:wanderly_app/_mock/trip_mock.dart';
 import 'package:wanderly_app/theme/app_colors.dart';
 import 'package:wanderly_app/theme/icon_sets.dart';
+import 'package:wanderly_app/widgets/bottom_dialog.dart';
+import 'package:wanderly_app/widgets/button_alternative.dart';
+import 'package:wanderly_app/widgets/trip_card.dart';
 
-class TripDetailScreen extends StatefulWidget {
+class TripDetailScreen extends ConsumerStatefulWidget {
   static const routeName = 'trip_detail';
   final int tripId;
 
   const TripDetailScreen({super.key, required this.tripId});
 
   @override
-  State<TripDetailScreen> createState() => _TripDetailScreenState();
+  ConsumerState<TripDetailScreen> createState() => _TripDetailScreenState();
 }
 
-class _TripDetailScreenState extends State<TripDetailScreen> {
+class _TripDetailScreenState extends ConsumerState<TripDetailScreen> {
   late Trip trip;
   bool _isLiked = false;
 
@@ -31,199 +35,273 @@ class _TripDetailScreenState extends State<TripDetailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: 403,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(trip.imagePath),
-                      fit: BoxFit.cover,
-                    ),
-                    borderRadius: BorderRadius.circular(34),
-                  ),
-                ),
-
-                Positioned(
-                  top: 20,
-                  left: 17,
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Container(
-                      width: 54,
-                      height: 54,
-                      decoration: BoxDecoration(
-                        color: AppColors.light.primary,
-                        borderRadius: BorderRadius.circular(34),
-                      ),
-                      child: Icon(
-                        Icons.arrow_back_ios_new_rounded,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-
-                Positioned(
-                  bottom: 16,
-                  right: 16,
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        _isLiked = !_isLiked;
-                      });
-                    },
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(50),
-                      child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white.withAlpha(30),
-                            borderRadius: BorderRadius.circular(50),
-                            border: Border.all(
-                              color: Colors.white.withAlpha(50),
-                              width: 1,
-                            ),
-                          ),
-                          width: 70,
-                          height: 70,
-                          child: Center(
-                            child: Iconify(
-                              likeIcon,
-                              size: 38,
-                              color: _isLiked ? Colors.pink : Colors.grey,
-                            ),
-                          ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(8, 30, 8, 55),
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        height: 403,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          image: trip.imagePath.isEmpty
+                              ? null
+                              : DecorationImage(
+                                  image: AssetImage(trip.imagePath),
+                                  fit: BoxFit.cover,
+                                ),
+                          color: trip.imagePath.isEmpty
+                              ? Colors.grey[200]
+                              : null,
+                          borderRadius: BorderRadius.circular(34),
                         ),
+                        child: trip.imagePath.isEmpty
+                            ? Center(
+                                child: Icon(
+                                  Icons.image_not_supported_outlined,
+                                  color: Colors.grey[400],
+                                  size: 64,
+                                ),
+                              )
+                            : null,
                       ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
 
-            SizedBox(height: 23),
-
-            Padding(
-              padding: const EdgeInsets.only(left: 17),
-              child: Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      spacing: 5,
-                      children: [
-                        Icon(
-                          Icons.location_on_rounded,
-                          size: 19,
-                          color: AppColors.light.primary,
-                        ),
-                        Text(
-                          trip.location,
-                          style: GoogleFonts.quicksand(
-                            textStyle: TextStyle(
+                      Positioned(
+                        top: 20,
+                        left: 17,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            width: 54,
+                            height: 54,
+                            decoration: BoxDecoration(
                               color: AppColors.light.primary,
-                              fontWeight: FontWeight.w600,
-                              fontSize: 14,
+                              borderRadius: BorderRadius.circular(34),
+                            ),
+                            child: Icon(
+                              Icons.arrow_back_ios_new_rounded,
+                              color: Colors.white,
                             ),
                           ),
                         ),
-                      ],
-                    ),
-
-                    Text(
-                      trip.title,
-                      style: GoogleFonts.raleway(
-                        textStyle: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: AppColors.light.textPrimary,
-                          fontSize: 32,
-                        ),
                       ),
-                    ),
 
-                    SizedBox(height: 16),
-                    Text(
-                      '"${trip.description}"',
-                      style: GoogleFonts.quicksand(
-                        textStyle: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
-
-                    SizedBox(height: 22),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Icon(Icons.star_rounded, size: 20, color: Colors.amber),
-                        Text(
-                          trip.rating.toString(),
-                          style: GoogleFonts.quicksand(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                        Container(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 10,
-                            vertical: 7,
-                          ),
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 224, 236, 255),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Row(
-                            spacing: 6,
-                            children: [
-                              trip.category.categoryIcon,
-                              Text(
-                                trip.category.title,
-                                style: GoogleFonts.quicksand(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: AppColors.light.primary,
+                      Positioned(
+                        bottom: 16,
+                        right: 16,
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _isLiked = !_isLiked;
+                            });
+                          },
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white.withAlpha(30),
+                                  borderRadius: BorderRadius.circular(50),
+                                  border: Border.all(
+                                    color: Colors.white.withAlpha(50),
+                                    width: 1,
+                                  ),
+                                ),
+                                width: 70,
+                                height: 70,
+                                child: Center(
+                                  child: Iconify(
+                                    likeIcon,
+                                    size: 38,
+                                    color: _isLiked ? Colors.pink : Colors.grey,
+                                  ),
                                 ),
                               ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                        Icon(
-                          Icons.reviews_rounded,
-                          size: 18,
-                          color: AppColors.light.primary,
-                        ),
-                        SizedBox(width: 6),
-                        Text(
-                          trip.reviews.toString(),
-                          style: GoogleFonts.quicksand(
-                            textStyle: TextStyle(
-                              fontSize: 14,
-                              color: AppColors.light.textPrimary,
-                              fontWeight: FontWeight.w600,
                             ),
                           ),
                         ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 23),
+
+                  Padding(
+                    padding: const EdgeInsets.only(left: 17),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          spacing: 5,
+                          children: [
+                            Icon(
+                              Icons.location_on_rounded,
+                              size: 19,
+                              color: AppColors.light.primary,
+                            ),
+                            Text(
+                              trip.location,
+                              style: GoogleFonts.quicksand(
+                                textStyle: TextStyle(
+                                  color: AppColors.light.primary,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        Text(
+                          trip.title,
+                          style: GoogleFonts.raleway(
+                            textStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.light.textPrimary,
+                              fontSize: 32,
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: 16),
+                        Text(
+                          '"${trip.description}"',
+                          style: GoogleFonts.quicksand(
+                            textStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: 22),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.star_rounded,
+                              size: 20,
+                              color: Colors.amber,
+                            ),
+                            Text(
+                              trip.rating.toString(),
+                              style: GoogleFonts.quicksand(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 7,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 224, 236, 255),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Row(
+                                spacing: 6,
+                                children: [
+                                  trip.category.categoryIcon,
+                                  Text(
+                                    trip.category.title,
+                                    style: GoogleFonts.quicksand(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: AppColors.light.primary,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            Icon(
+                              Icons.reviews_rounded,
+                              size: 18,
+                              color: AppColors.light.primary,
+                            ),
+                            SizedBox(width: 6),
+                            Text(
+                              trip.reviews.toString(),
+                              style: GoogleFonts.quicksand(
+                                textStyle: TextStyle(
+                                  fontSize: 14,
+                                  color: AppColors.light.textPrimary,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        SizedBox(height: 25),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Rekomendasi Trip Kami",
+                              style: GoogleFonts.raleway(
+                                textStyle: TextStyle(
+                                  color: AppColors.light.textPrimary,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+
+                            Column(
+                              children: () {
+                                final trips = tripMockData.take(3).toList();
+                                trips.shuffle();
+                                return trips
+                                    .map(
+                                      (trip) => TripCard(
+                                        imagePath: trip.imagePath,
+                                        title: trip.title,
+                                        location: trip.location,
+                                        category: trip.category.title,
+                                        categoryIcon:
+                                            trip.category.categoryIcon,
+                                        rating: trip.rating,
+                                        reviews: trip.reviews,
+                                      ),
+                                    )
+                                    .toList();
+                              }(),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: ButtonColor(
+                text: "Tambah Trip",
+                width: double.infinity,
+                onPressed: () {
+                  showBottomDialog(context, ref);
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
